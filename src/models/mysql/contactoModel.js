a3177e8a6ed23f4a46f582c770cd769298f9ea2f
Agustin <agustinyedro@gmail.com>
2024-06-19 17:50:06 -0300

    se agrega base de datos

--- src/models/mysql/contactoModel.js
+++ src/models/mysql/contactoModel.js
@@ -1,43 +1,43 @@
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise");
 /* https://www.clever-cloud.com */
 const config = {
-    host: 'databases.000webhost.com',
-    user: 'id22295146_node4',
-    password: 'Gruponode4.-',
-    database: 'id22295146_node4',
-    connectTimeout: 10000
+  host: "bwjmdpdj9q7rrxqh1rfb-mysql.services.clever-cloud.com",
+  port: 3306,
+  user: "ubuscb4oypdelyce",
+  password: "SFHTspAPvwYLzgUoFyrI.-",
+  database: "bwjmdpdj9q7rrxqh1rfb",
 };
 
 async function connectToDatabase() {
-    try {
-        const connection = await mysql.createConnection(config);
-        console.log('Conexión exitosa a la base de datos');
-        return connection;
-    } catch (error) {
-        console.error('Error al conectar a la base de datos:')
-        throw error;
-    }
+  try {
+    const connection = await mysql.createConnection(config);
+    console.log("Conexión exitosa a la base de datos");
+    return connection;
+  } catch (error) {
+    console.error("Error al conectar a la base de datos:");
+    throw error;
+  }
 }
 
 module.exports = connectToDatabase;
 
 class contactoModel {
-    static async getAll({ genre }) {
-        const conn = await connectToDatabase();
-        const [result] = await conn.query(
-            'SELECT id_consulta, nombre, email, telefono, motivo, mensage, medio FROM consulta;'
-        );
-        console.log(result);
-        // return result
-    }
+  static async getAll({ genre }) {
+    const conn = await connectToDatabase();
+    const result = await conn.query(
+      "SELECT id_consulta, nombre, email, telefono, motivo, mensage, medio FROM Consulta;"
+    );
+    console.log(result);
+    // return result
+  }
 
-    static async getById({ id }) { }
+  static async getById({ id }) {}
 
-    static async create({ input }) { }
+  static async create({ input }) {}
 
-    static async delete({ id }) { }
+  static async delete({ id }) {}
 
-    static async update({ id, input }) { }
+  static async update({ id, input }) {}
 }
 
 module.exports = contactoModel;
